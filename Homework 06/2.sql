534655d8a64a83c6cd3a7a410f745bf6c8b91117
Dmitry Popov <supernic3@protonmail.com>
2019-06-17 17:58:43 +0300

    Fix homework #6 (second query)

--- Homework 06/2.sql
+++ Homework 06/2.sql
@@ -1,22 +1,24 @@
 --2. Вывести список 2х самых популярных продуктов (по кол-ву проданных) в каждом месяце за 2016й год (по 2 самых популярных продукта в каждом месяце)
-SELECT *
-FROM
-(
-    SELECT subq.StockItemName, 
-           subq.TotalQuantity, 
-           subq.MonthNumber, 
-           ROW_NUMBER() OVER(PARTITION BY subq.MonthNumber ORDER BY subq.TotalQuantity DESC) AS [Num]
-    FROM
-    (
-        SELECT si.StockItemName, 
-               SUM(il.Quantity) AS [TotalQuantity], 
-               DATEPART(MONTH, i.InvoiceDate) AS [MonthNumber]
-        FROM Sales.InvoiceLines il
-             INNER JOIN [Sales].[Invoices] i ON il.InvoiceID = i.InvoiceID 
-             INNER JOIN Warehouse.StockItems si ON il.StockItemID = si.StockItemID
-		WHERE DATEPART(YEAR, i.InvoiceDate) = 2016
-        GROUP BY si.StockItemName, DATEPART(MONTH, i.InvoiceDate)
-    ) AS subq
-) AS subq2
-WHERE subq2.Num <= 2
-ORDER BY subq2.MonthNumber,  subq2.TotalQuantity DESC;
+WITH ProductMonthSales
+AS (
+    SELECT si.StockItemName, 
+        SUM(il.Quantity) AS [TotalQuantity], 
+        MONTH(i.InvoiceDate) AS [MonthNumber]
+    FROM Sales.InvoiceLines il
+        INNER JOIN [Sales].[Invoices] i ON il.InvoiceID = i.InvoiceID 
+        INNER JOIN Warehouse.StockItems si ON il.StockItemID = si.StockItemID
+	WHERE YEAR(i.InvoiceDate) = 2016
+    GROUP BY si.StockItemName, MONTH(i.InvoiceDate)
+),
+ProductSalesNumbered
+AS (
+    SELECT ProductMonthSales.StockItemName, 
+        ProductMonthSales.TotalQuantity, 
+        ProductMonthSales.MonthNumber, 
+        ROW_NUMBER() OVER(PARTITION BY ProductMonthSales.MonthNumber ORDER BY ProductMonthSales.TotalQuantity DESC) AS [Num]
+    FROM ProductMonthSales
+)
+SELECT psm.StockItemName, psm.TotalQuantity
+FROM ProductSalesNumbered psm
+WHERE psm.Num <= 2
+ORDER BY psm.MonthNumber, psm.TotalQuantity DESC;
